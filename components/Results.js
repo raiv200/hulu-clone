ef6205df4b401bea5817fdcfcf4120375dc7716e
vikas <raiv2004322@gmail.com>
2021-04-23 19:46:37 +0530

    first commit

---
+++ components/Results.js
@@ -0,0 +1,16 @@
+import Thumbnail from "./Thumbnail";
+import FlipMove from "react-flip-move";
+
+function Results({ results }) {
+
+   
+  return (
+    <FlipMove className="mx-5 py-10 sm:grid md:grid-cols-2 xl:grid-cols-4 3xl:flex flex-wrap justify-center">
+      {results.map((result) => (
+        <Thumbnail key={result.id} result={result} />
+      ))}
+    </FlipMove>
+  );
+}
+
+export default Results;
